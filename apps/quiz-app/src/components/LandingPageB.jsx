c4fa3ba0fa6d294e88135089bf0d72ec6137909d
Shobha <shobha@MacBook-Air.local>
2026-03-04 16:29:18 +0100

    update

--- apps/quiz-app/src/components/LandingPageB.jsx
+++ apps/quiz-app/src/components/LandingPageB.jsx
@@ -96,13 +96,13 @@ export default function LandingPageB() {
 
           <div className="landing-b__ctas">
             <Link to="/play/free" className="landing-b__cta-primary">
-              Host a Quiz
+              Play a Quiz
             </Link>
             <button
               className="landing-b__cta-outline"
               onClick={handleUnlockPacks}
             >
-              &#128274; Play a Quiz
+              &#128274; Host a Quiz
             </button>
           </div>
 
